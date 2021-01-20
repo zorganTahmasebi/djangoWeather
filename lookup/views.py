2ddfde301eb0d75b125f4197e21bbbb9bdffb3d2
amin tahmasebi <amintahmasebi479@gmail.com>
2021-01-20 22:17:24 +0330

    set apiair and get response

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 # This is my views in djnago
 
 def home(request):
-    return render(request, 'home.html', {})
+    # https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=~distance~&API_KEY=9034563F-7773-4C12-92E0-E1DDD216565A
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=9034563F-7773-4C12-92E0-E1DDD216565A");
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api' : api})
  
 
 def about(request):
